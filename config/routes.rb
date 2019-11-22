314f0e037bcf0f846bfe1788982ac84f886ccbbb
pquezada <pquezada2009@hotmail.es>
2019-11-22 13:04:59 -0300

    session controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'users/show'
   root 'users#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+    }
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
